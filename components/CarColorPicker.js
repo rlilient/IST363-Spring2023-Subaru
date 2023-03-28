fe0b2c17222bd1643d218313cc9104906e22dc5d
rlilient <rlilient@syr.edu>
2023-03-28 18:26:20 -0400

    more work w color picker

---
+++ components/CarColorPicker.js
@@ -0,0 +1,22 @@
+import Swatches from './Swatches';
+import { useState } from 'react';
+import Image from 'next/image';
+
+const CarColorPicker = ({ colors }) => {
+    const [activeColor, setActiveColor] = useState(colors[0]);
+
+    return <div>
+        <h2>color picker</h2>
+        <div>
+            <Image 
+                src={`/vehicles/crosstrek/colors/crosstrek-${activeColor.slug}.webp`}
+                alt={`Crosstrek ${activeColor.name}`}
+                width={300}
+                height={200}
+            />
+        </div>
+        <Swatches colors={colors} />
+        <h3>color name</h3>
+        </div>
+}
+export default CarColorPicker;
